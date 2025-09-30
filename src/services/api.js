c5d0c8b287e219df13308b296d6aa82c3c06613f
RodrigoMeloads <augmelo.rodrigo@gmail.com>
2025-09-30 16:14:10 -0300

    Atualiza api.js para variável de ambiente

--- src/services/api.js
+++ src/services/api.js
@@ -3,9 +3,9 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "0923ac32d20ebd91568b4141bf65e2f5", // sua chave
-    language: "pt-BR"
-  }
+    api_key: import.meta.env.VITE_TMDB_API_KEY,
+    language: "pt-BR",
+  },
 });
 
 export default api;
